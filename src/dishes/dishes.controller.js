c34e7e4388af5e002893aad0b8bcc80c1ef65649
Devon <41218582+Devon-Carroll@users.noreply.github.com>
2023-10-17 19:06:50 -0400

    Update dishes.controller.js

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -2,12 +2,25 @@ const path = require("path");
 
 // Use the existing dishes data
 const dishes = require(path.resolve("src/data/dishes-data"));
-
+const validateDishData = require("../middleware/validateDishData");
 // Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /dishes handlers needed to make the tests pass
 
+
+// TODO: Implement the /dishes handlers needed to make the tests pass
+function dishExists(req, res, next) {
+  const { dishId } = req.params;
+  const foundDish = dishes.find((dish) => dish.id === dishId);
+  if(foundDish) {
+    res.locals.dish = foundDish;
+    return next();
+  }
+  next({
+    status: 404,
+    message: `Dish id not found: ${dishId}`,
+  })
+}
 
 // Handler for creating a new dish
 function createDish(req, res) {
@@ -48,54 +61,20 @@ function listDishes(req, res) {
 
 // Handler for reading a specific dish by ID
 function readDish(req, res) {
-  const { dishId } = req.params;
-  const dish = dishes.find((dish) => dish.id === dishId);
-
-  if (dish) {
-    res.status(200).json({ data: dish });
-  } else {
-    res.status(404).json({ error: 'Dish not found.' });
-  }
+  res.json(({ data: res.locals.dish }))
 }
 
 // Handler for updating a specific dish by ID
 function updateDish(req, res) {
-  const { dishId } = req.params;
-  const { data } = req.body;
-
-  // Find the dish by ID
-  const dish = dishes.find((dish) => dish.id === dishId);
-
-  if (!dish) {
-    return res.status(404).json({ error: `Dish with ID ${dishId} not found.` });
-  }
-
-  // Check if data.id exists and matches :dishId in the route
-  if (data.id && data.id !== dishId) {
-    return res.status(400).json({ error: `data id ${data.id} does not match the dish id ${dishId}` });
-  }
-
-  // Validate the incoming data (similar to createDish validation)
-  const { name, description, price, image_url } = data;
-    if (!name) {
-      return res.status(400).json({ error: 'name is missing' });
-    } else if (!description) {
-      return res.status(400).json({ error: 'description is missing' });
-    } else if (!price) {
-      return res.status(400).json({ error: 'price is missing' });
-    } else if (price < 0) {
-      return res.status(400).json({ error: 'price is missing' });
-    } else if (typeof price !== 'number'){
-      return res.status(400).json({ error: 'price must be a number'});
-    } else if (!image_url) {
-      return res.status(400).json({ error: 'image_url is missing' });
-    }
+  const dish = res.locals.dish;
+  const { data: { name, description, price, image_url } = {} } = req.body;
+  dish.name = name;
+  dish.description = description;
+  dish.price = price;
+  dish.image_url = image_url;
   
-
-  // Update the dish
-  Object.assign(dish, data);
-
-  res.status(200).json({ data: dish });
+  res.json({data: dish })
+  
 }
 
 
@@ -109,7 +88,12 @@ function destroy(req, res, next) {
 module.exports = {
   createDish,
   listDishes,
-  readDish,
-  updateDish,
+  readDish: [dishExists, readDish],
+  updateDish: [
+    dishExists,
+    validateDishData,
+    updateDish,
+    
+  ],
   delete: destroy,
 };
